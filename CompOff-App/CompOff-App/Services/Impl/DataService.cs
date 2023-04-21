05d1558451a815284a2ce1ce302c09eb14049dad
adroob19 <62326343+adroob19@users.noreply.github.com>
2023-04-21 10:27:27 +0200

    Added DataService

---
+++ CompOff-App/CompOff-App/Services/Impl/DataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompOff_App.Services.Impl
+{
+    class DataService : IDataService
+    {
+    }
+}
