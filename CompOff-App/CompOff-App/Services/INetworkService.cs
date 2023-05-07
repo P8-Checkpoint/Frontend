5891b5775803ed076e8c35a6490c32a3cd731b40
Magnus Mathiesen <magnus_mat@pm.me>
2023-05-08 00:06:07 +0200

    Added Network Service

---
+++ CompOff-App/CompOff-App/Services/INetworkService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompOff_App.Services;
+
+internal interface INetworkService
+{
+    void ConnectToNetwork(string networkSsid, string networkPassword);
+    void DisconnectFromNetwork(string networkSsid);
+    string GetNetworkSsid();
+    void FindNearbyNetworks();
+}
