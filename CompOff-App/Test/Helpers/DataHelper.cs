e01b42bfb9de053b17e5e92da0b6b00a460590aa
adroob19 <62326343+adroob19@users.noreply.github.com>
2023-05-17 22:47:36 +0200

    Tests

---
+++ CompOff-App/Test/Helpers/DataHelper.cs
@@ -0,0 +1,39 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Helpers;
+
+internal static class DataHelper
+{
+    public static Guid DummyGuid(int n)
+    {
+        return n switch
+        {
+            1 => Guid.Parse("4e096c77-0774-4b80-8d0c-b86a17f3c512"),
+            2 => Guid.Parse("3f5e96e8-d180-4e6b-b5d6-02a732d7374e"),
+            3 => Guid.Parse("45650fbf-bba3-4016-a801-5cc90f09ec86"),
+            4 => Guid.Parse("24172d0a-d5ef-41df-a837-3d03264116a0"),
+            _ => Guid.Parse("b9f2ab8f-d3c1-49ef-a39a-c0b7c4bf38ad")
+        };
+    }
+
+    public static User GetUser(int n)
+    {
+        return n switch
+        {
+            1 => new User("Jeff", "Winger", "Tango"),
+            2 => new User("Abed", "Nadir", "Inspector"),
+            3 => new User("Troy", "Barnes", "T-Bone"),
+            4 => new User("Britta", "Perry", "The_Worst"),
+            5 => new User("Annie", "Eddison", "Annie_Adderal"),
+            6 => new User("Pierce", "Hawthorne", "Anastasia"),
+            7 => new User("Shirley", "Bennet", "Big_Cheddar"),
+            _ => new User("Ben", "Chang", "El_Tigre_Chino"),
+
+        };
+    }
+}
---
+++ CompOff-App/Test/Viewmodels/Tabs/JobListPageViewModelTest.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Viewmodels.Tabs;
+using Wrappers;
+using Tests.Helpers;
+
+namespace Tests.Viewmodels.Tabs;
+
+public class JobListPageViewModelTest
+{
+    private readonly JobListPageViewModel _sut;
+    private readonly Mock<INavigationWrapper> _navigatorMock;
+    private readonly Mock<IDataService> _dataServiceMock;
+
+    public JobListPageViewModelTest()
+    {
+        _navigatorMock = new Mock<INavigationWrapper>();
+        _dataServiceMock = new Mock<IDataService>();
+
+        _sut = new JobListPageViewModel(_navigatorMock.Object, _dataServiceMock.Object);
+    }
+
+    [Fact]
+    public void JobClicked_CheckCommandExistence_NotNull()
+    {
+        Assert.NotNull(_sut.JobClickedCommand);
+    }
+
+    [Fact]
+    public async Task InitializeAsync_SetsCurrentUser_ExpectCurrentUserSetAsync()
+    {
+        var expected = DataHelper.GetUser(1).UserName;
+        _sut.CurrentUser = null;
+
+        _dataServiceMock.Setup(mock => mock.GetCurrentUserAsync()).ReturnsAsync(DataHelper.GetUser(1));
+
+        await _sut.InitializeAsync();
+
+        var actual = _sut.CurrentUser?.UserName;
+
+        Assert.Equal(expected, actual);
+    }
+}
